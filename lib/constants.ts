050693eb58a38b1d557deeea450b65fc7343a1d8
Faraz <faraznaseem789@gmail.com>
2026-04-18 16:55:10 +0530

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,50 @@
+export const events = [
+    {
+        title: "Next.js Conf 2026",
+        image: "/images/event1.png",
+        slug: "nextjs-conf-2026",
+        location: "San Francisco, CA",
+        date: "October 24, 2026",
+        time: "10:00 AM"
+    },
+    {
+        title: "React Summit",
+        image: "/images/event2.png",
+        slug: "react-summit",
+        location: "Amsterdam, NL",
+        date: "June 12, 2026",
+        time: "09:00 AM"
+    },
+    {
+        title: "Web Performance Show",
+        image: "/images/event3.png",
+        slug: "web-performance-show",
+        location: "London, UK",
+        date: "August 18, 2026",
+        time: "11:00 AM"
+    },
+    {
+        title: "Global AI Hackathon",
+        image: "/images/event4.png",
+        slug: "global-ai-hackathon",
+        location: "Remote / Online",
+        date: "May 15-17, 2026",
+        time: "All Day"
+    },
+    {
+        title: "TypeScript Congress",
+        image: "/images/event5.png",
+        slug: "typescript-congress",
+        location: "Berlin, DE",
+        date: "September 10, 2026",
+        time: "09:30 AM"
+    },
+    {
+        title: "Modern CSS Workshop",
+        image: "/images/event6.png",
+        slug: "modern-css-workshop",
+        location: "New York, NY",
+        date: "November 05, 2026",
+        time: "01:00 PM"
+    }
+];
